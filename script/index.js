5e1b1d4514fb4fc9c0151d7b879aaf9166f861c3
Ирина Кучина <irkuchir@mail.ru>
2022-03-24 21:08:23 +0500

    ready lesson

---
+++ script/index.js
@@ -0,0 +1,6 @@
+'use strict';
+
+const header = prompt('Заполните поле');
+const form = prompt('Заполните поле');
+const checkbox = prompt('Заполните поле');
+const checkboxField = prompt('Заполните поле');
